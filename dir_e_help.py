04a86dc27cac280b66306dd2d6dc246f335b8ecb
Christian Castro <alvescastro00cca13@gmail.com>
2020-05-31 02:22:03 -0300

    s3ep11

---
+++ dir_e_help.py
@@ -0,0 +1,17 @@
+"""
+Utilitários para auxiliar na programação
+
+dir -> Apresenta todos os atributos/propriedades e funções/métodos dispiníveis para determinado tipo de dado ou
+
+dir(tipo de dado/variável)
+ex: "university".upper() -> UNIVERSITY
+
+
+help -> Apresenta a documentação/como utilizar os atributos/propriedades e funções/métodos disponíveis para
+        determinado tipo de dado ou variável.
+dir(tipo de dado/variável.propriedade)
+ex: help("Geek",upper)
+"""
+
+
+print(dir("progpy"))
